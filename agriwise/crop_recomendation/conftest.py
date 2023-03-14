d6d0f6f05e74f8c96d83ea75c1b3a85fa93e9e9a
shroukhegazi <shroukhegazi99>
2023-03-14 03:51:40 +0200

    fixed linter issuse

--- agriwise/crop_recomendation/conftest.py
+++ agriwise/crop_recomendation/conftest.py
@@ -15,14 +15,14 @@ def user(db) -> User:
 
 @pytest.fixture
 def super_user(db) -> User:
-    user= User.objects.create_superuser(
+    user = User.objects.create_superuser(
         username="username1",
         email="username1@email.com",
         password="sh0123456789123456",
     )
     user.is_superuser = True
     return user
-     
+
 
 @pytest.fixture
 def auth_client(user):
@@ -36,5 +36,3 @@ def auth_client_super_user(super_user):
     client = APIClient()
     client.force_authenticate(super_user)
     return client
-
-    
