efc4bd576a07dc9a72140a1b0819681c7de8bc82
shroukhegazi <shroukhegazi99@gmail.c0m>
2023-04-30 13:22:35 +0300

    removed unneeded code

--- agriwise/agriculture_specialist/helpers.py
+++ agriwise/agriculture_specialist/helpers.py
@@ -1,7 +1,5 @@
 from django.core.mail import EmailMessage
 from rest_framework.exceptions import APIException
-from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 
 
 def send_email(subject, message, recipient_list):
@@ -12,6 +10,5 @@ def send_email(subject, message, recipient_list):
             to=recipient_list,
         )
         email.send()
-        return Response({"message": "Email sent successfully."}, status=HTTP_200_OK)
     except Exception as e:
-        raise APIException(detail=str(e), code=HTTP_400_BAD_REQUEST)
+        raise APIException(detail=str(e))
