20d5521321adf09e0ddd03ec195fe7ecaba5fa9c
shroukhegazi <shroukhegazi99@gmail.c0m>
2023-05-25 12:36:25 +0300

    change media url, update soil type models

--- agriwise/soil_type/models.py
+++ agriwise/soil_type/models.py
@@ -11,5 +11,5 @@ class SoilImage(models.Model):
 class SoilType(models.Model):
     soil_type = models.CharField(max_length=200)
     created_at = DateTimeField(auto_now=True)
-    soil_image = ForeignKey(SoilImage, on_delete=models.SET_NULL, null=True)
+    soil_image = ForeignKey(SoilImage, on_delete=models.CASCADE, null=True)
     user = ForeignKey(User, on_delete=models.SET_NULL, null=True)
